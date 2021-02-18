ba19e781357b1701435413ab0bdc2e39aa9ccc3b
Michael Probst <mpprobst@gmail.com>
2021-02-18 08:14:08 -0500

    Add project

---
+++ spanning tree/spanning tree/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp
+// PURPOSE: central file for running the spanning tree algorithm
+// AUTHOR: Michael Probst
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string input = "";
+	cout << "What is your name?";
+	cin >> input;
+	cout << "Did you say you name is Butt Poopington>? That's a silly name." << endl;
+}
